2c1f9bbe50b533a6e5dde2adf3b7137f0bbbfdd2
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-07-06 20:27:27 +0530

    Add files via upload

---
+++ Asciivalue.java
@@ -0,0 +1,10 @@
+//program to find a ascii value of a character
+public class Asciivalue {
+    public static void main(String[] args) {
+         char ch='b';
+         int ascii=ch;
+         int castAscii=(int)ch;
+         System.out.println("The Ascii value of"+ ch+"is:"+ascii);
+         System.out.println("The Ascii value of"+ ch+"is:"+castAscii);
+    }
+}
