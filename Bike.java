5b2832602419214bdbbcb7ece3ee444eab5902c6
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:20:54 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,13 @@
+ //default constructor
+ class Bike {
+    Bike()
+    {
+        System.out.println("love to drive a bike");
+    }
+    }
+class Sample
+{
+    public static void main(String[] args) {
+        new Bike();
+    }
+}
