b7717f473559890f301fe67ff513d83e7e914a78
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:22:00 +0530

    Add files via upload

---
+++ Colour.java
@@ -0,0 +1,12 @@
+//enum program
+enum Colour {
+    RED,YELLOW,PINK,BLUE;
+
+    }   
+class ChooseColour
+{
+  public static void main(String[] args) {
+    Colour c=Colour.PINK;
+    System.out.println(c);
+  }
+}
