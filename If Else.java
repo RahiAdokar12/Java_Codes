d22c9e369e2d550c901693c9381454c00f77eee6
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:33:58 +0530

    Add files via upload

---
+++ If Else.java
@@ -0,0 +1,15 @@
+ class IfElse {
+    public static void main(String args[])
+     {
+        int marks=20;
+        if (marks>=30) 
+        {
+            System.out.println("Pass");
+        } else 
+        {
+           System.out.println("Fail"); 
+        }
+        System.out.println("outside the loop");
+    }
+    
+}
