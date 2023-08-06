407fdfe8538ca1d5370cfbb3e8fa7d967727434f
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 18:01:43 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,12 @@
+//summation of no.s from 1 to 10
+class Whileloop {
+    public static void main(String[] args) {
+        int x=1,sum=0;
+       while(x<=10)
+       {
+          sum++;
+          x++;
+          System.out.println("sum:"+sum);
+       }
+    }
+}
