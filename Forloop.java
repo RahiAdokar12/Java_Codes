7c0b0bd4778b2ba051c3de2a50ef6cff6e6ee9fe
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:32:42 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,11 @@
+//print the sum of x ranging from 1 to 20
+class Forloop {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int x=1;x<=20;x++)
+        {
+            sum++;
+            System.out.println("sum:"+sum);
+        }
+    }
+}
