30f4d00b82557b225a5b07ed1e5755c38303c3b4
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:34:29 +0530

    Add files via upload

---
+++ If Else If.java
@@ -0,0 +1,15 @@
+class IfElseIf
+{
+   public static void main(String args[]) 
+   {
+    int CET=60;
+    int JEE=60;
+    if (CET>=50) 
+    {
+        System.out.println("Eligible in Cet");
+    } else if(JEE>=60);
+     {
+        System.out.println("Eligible in Jee");
+    }
+   }
+}
