935b97e481062b34bd3cf03eccdd99ad1a6f7fce
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:35:03 +0530

    Add files via upload

---
+++ If Else 2.java
@@ -0,0 +1,14 @@
+ class IfElse2
+ {
+    public static void main(String[] args)
+     {
+        int Dip=40;
+        int Cet=10;
+        if (Dip>=50 || Cet>=20) 
+        {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
+    }
+}
