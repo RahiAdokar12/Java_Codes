5d2d4d89932a076df62acf3354071ec1b87541f7
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:36:47 +0530

    Add files via upload

---
+++ If Else1.java
@@ -0,0 +1,21 @@
+class IfElse1
+{
+    public static void main(String args[]) 
+    {
+        int SSC=50;
+        int HSC=10;
+        if (SSC>=40) 
+        {
+            if (HSC>=40) 
+            {
+                System.out.println("Eligible for Admission");
+            } else 
+            {
+                System.out.println(" not Eligible for Admission");
+            }
+        } else 
+        {
+            System.out.println("  Eligible for Admission");
+        }
+    }
+}
