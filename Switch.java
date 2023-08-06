9244f686da07c440bfcece7ff4cf84578d818d9c
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:53:04 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,22 @@
+ class Switch {
+    public static void main(String[] args) {
+    int month=4;
+    switch (month)
+    {
+        case 1:
+        System.out.println("Jan");
+        break;
+        case 2:
+        System.out.println("Feb");
+        break;
+        case 3:
+        System.out.println("March");
+        break;
+        case 4:
+        System.out.println("April");
+        break;
+        default:
+        System.out.println("any month");
+    }
+}
+ }
