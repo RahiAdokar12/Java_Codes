482a4ebfc4aa6b828ef75b1c30ed0272e6bd4b79
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:53:27 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,11 @@
+class Ternary
+{
+public static void main(String args[])
+{
+int num1=20;
+int num2=40;
+int max;
+max=(num1>num2) ? num1:num2;
+System.out.println("Largest Number is:" +max);
+}
+}
