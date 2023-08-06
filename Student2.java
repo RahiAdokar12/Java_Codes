9fb46196e3b2019bb7e6f95ae9cd4d9eb2906313
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:47:47 +0530

    Add files via upload

---
+++ Student2.java
@@ -0,0 +1,14 @@
+// Initialization through reference
+class Student2 {
+    int id;
+    String name;
+    }
+class TestStudent1
+{
+    public static void main(String[] args)
+     {
+    Student2 s2=new Student2();
+    s2.id=200;
+    s2.name="Eisha";
+    System.out.println(s2.id+" "+s2.name);
+   } }
