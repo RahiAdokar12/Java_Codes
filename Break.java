94a428b23218858afd7acae607a93e6655f5a23e
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:21:15 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,13 @@
+class Break {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            if(i==3)
+            {
+                System.out.println("breaking the loop");
+                break;
+            }
+            System.out.println("Hello world");
+        }
+    }
+}
