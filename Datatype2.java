778a962ad89a08a175ca830d33c8aac4c3979ca3
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:26:49 +0530

    Add files via upload

---
+++ Datatype2.java
@@ -0,0 +1,12 @@
+ class Datatype2 {
+    public static void main (String args[]) 
+    {
+       // byte a=9;
+          //int b=a;
+       short a=6;
+        byte b=(byte)a;
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+    }
+}
+
