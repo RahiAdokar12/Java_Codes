99e244099a60780596584f7cc514c91235f6287b
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:39:16 +0530

    Add files via upload

---
+++ If Else3.java
@@ -0,0 +1,15 @@
+ class IfElse3
+ {
+    
+    public static void main(String[] args)
+     {
+        int Dip=40;
+        int Cet=20;
+        if (Dip>=50 && Cet>=20) 
+        {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
+    }
+}
