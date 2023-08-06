65545069f7f452b1834cfd30fc45e7ba417f12c5
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:27:21 +0530

    Add files via upload

---
+++ Even 1.java
@@ -0,0 +1,16 @@
+//interval between 1 to 20 even
+class Twenthy
+ {
+    public  static void main(String args[])
+     {
+        int num1=1;
+        int num2=20;
+        for(int i=num1;i<=num2;i++)
+        {
+           if(i%2==0)
+           {
+            System.out.println("Number "+i+" is even");
+           }
+        }
+    }
+}
