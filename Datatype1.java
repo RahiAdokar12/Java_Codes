2e70274938fb3e80b9786497ffceddac2da0c364
RahiAdokar12 <118454009+RahiAdokar12@users.noreply.github.com>
2023-08-06 17:26:18 +0530

    Add files via upload

---
+++ Datatype1.java
@@ -0,0 +1,9 @@
+class Datatype1 {
+    public static void main(String args[]) 
+    {
+int q=5;
+System.out.println("The value of number is=");
+System.out.println(q);
+System.out.print("which is a small value");
+}
+}
